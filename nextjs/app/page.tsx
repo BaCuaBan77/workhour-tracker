4bf79000ac3392313acd03ede3712def7ccdb3d3
Long Le <long.le@observis.fi>
2023-09-15 22:53:25 +0300

    Dockerize the app

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,18 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import prisma from '../../prisma/prisma';
+import prisma from '../prisma';
+
 
 export default async function Home() {
-  const feed = await prisma.user.findFirst({
-    where: { name: 'Long Le' },
-  });
+  
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          {
-            feed?.email
-          }
+        
         </p>
         
       </div>
