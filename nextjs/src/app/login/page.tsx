96a625bbef3d5d9bf35e5d6dabbeb9dbbf0e220c
Long Le <long.le@observis.fi>
2023-11-15 20:21:18 +0200

    test dockerfile for production image

--- nextjs/src/app/login/page.tsx
+++ nextjs/src/app/login/page.tsx
@@ -5,7 +5,7 @@ import styles from '@/styles/login.module.css'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useAuth } from '@/context/AuthContext'
-import loginPageImage from '@/public/snowflake.jpg'
+import loginPageImage from '@/public/static/snowflake.jpg'
 
 const Login = () => {
   const { push } = useRouter()
