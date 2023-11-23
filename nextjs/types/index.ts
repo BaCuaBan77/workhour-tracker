da2ae3d24fa3af380cf1cfb3e07de88ab3b83cd5
Long Le <long.le@observis.fi>
2023-11-23 17:32:31 +0200

    hard coded instead

--- nextjs/types/index.ts
+++ nextjs/types/index.ts
@@ -46,7 +46,7 @@ export enum SystemUserPrivilege {
 
 export const API: string | undefined = process.env.NEXT_PUBLIC_SERVER_IP
   ? process.env.NEXT_PUBLIC_SERVER_IP
-  : 'keycloak'
+  : '192.168.0.187'
 
 export interface FullUserDTO {
   workHours?: {
